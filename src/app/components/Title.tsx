c6bb47a6bd8fe57daeca5c2ac9ef8c22f3ae1402
Dev Di Lauro <dev.dilauro@gmail.com>
2025-07-08 15:41:55 -0300

    fix: ajuste title desktop

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -15,29 +15,33 @@ export default function Title({
 
   return (
     <div className="container-fluid">
-      <div className="container d-flex justify-content-between align-items-end p-0">
-        <h1 className={`fs-6 fw-bold mb-1 ms-0 ps-0 `}>{children}</h1>
-        <div className="d-flex justify-content-end gap-2 flex-wrap">
-          {comp ? comp : null}
-          
-          {home ? (
-            <Link href={"/"} className="btn btn-sm btn-outline-light">
-              Home
-            </Link>
-          ) : null}
-          {link && control ? (
-            <Link href={link.path} className="btn btn-sm btn-outline-light">
-              {link.label}
-            </Link>
-          ) : null}
-          {back ? (
-            <button
-              onClick={() => router.back()}
-              className="btn btn-sm btn-outline-light"
-            >
-              Voltar
-            </button>
-          ) : null}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-3">
+            <h1 className={`fs-6 fw-bold mb-1 ms-0 ps-0 `}>{children}</h1>
+          </div>
+          <div className="col-9 d-flex justify-content-end gap-2 flex-wrap">
+            {comp ? comp : null}
+
+            {home ? (
+              <Link href={"/"} className="btn btn-sm btn-outline-light">
+                Home
+              </Link>
+            ) : null}
+            {link && control ? (
+              <Link href={link.path} className="btn btn-sm btn-outline-light">
+                {link.label}
+              </Link>
+            ) : null}
+            {back ? (
+              <button
+                onClick={() => router.back()}
+                className="btn btn-sm btn-outline-light"
+              >
+                Voltar
+              </button>
+            ) : null}
+          </div>
         </div>
       </div>
       <hr className="my-2" />
